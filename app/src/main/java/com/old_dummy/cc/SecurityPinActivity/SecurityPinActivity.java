a55a67c19567047b421265522d6179eac71a041b
Aman Kumar <amancodeservir@gmail.com>
2025-01-04 20:08:41 +0530

    history page updated

--- app/src/main/java/com/old_dummy/cc/SecurityPinActivity/SecurityPinActivity.java
+++ app/src/main/java/com/old_dummy/cc/SecurityPinActivity/SecurityPinActivity.java
@@ -11,9 +11,14 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -48,9 +53,9 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class SecurityPinActivity extends AppCompatActivity implements SecurityPinContract.View{
-
-    AppCompatCheckBox checkBox1,checkBox2,checkBox3,checkBox4;
+public class SecurityPinActivity extends AppCompatActivity implements SecurityPinContract.View {
+    EditText pinBox1, pinBox2, pinBox3, pinBox4;
+    CheckBox checkBox1, checkBox2, checkBox3, checkBox4;
     LinearLayout checkBoxLay;
     MaterialTextView resetPin;
     Vibrator vibrator;
@@ -84,28 +89,37 @@ public class SecurityPinActivity extends AppCompatActivity implements SecurityPi
     }
 
     private void intIDs() {
+        pinBox1 = findViewById(R.id.pinBox1);
+        pinBox2 = findViewById(R.id.pinBox2);
+        pinBox3 = findViewById(R.id.pinBox3);
+        pinBox4 = findViewById(R.id.pinBox4);
         checkBox1 = findViewById(R.id.checkBox1);
         checkBox2 = findViewById(R.id.checkBox2);
         checkBox3 = findViewById(R.id.checkBox3);
         checkBox4 = findViewById(R.id.checkBox4);
-        checkBoxLay = findViewById(R.id.checkBoxLay);
+        checkBoxLay = findViewById(R.id.pinCodeLayout);
         progressBar = findViewById(R.id.progressBar);
         resetPin = findViewById(R.id.resetPin);
         presenter = new SecurityPinPresenter(this);
         dataConText = findViewById(R.id.dataConText);
-        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE) ;
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         mobileNumber = getIntent().getStringExtra(getString(R.string.mobile_number));
         from = getIntent().getStringExtra("from");
-        presenter.doAppDetailsApi("");
 
+        addPinWatcher(pinBox1, checkBox1);
+        addPinWatcher(pinBox2, checkBox2);
+        addPinWatcher(pinBox3, checkBox3);
+        addPinWatcher(pinBox4, checkBox4);
+
+        presenter.doAppDetailsApi("");
         MaterialTextView topTitle = findViewById(R.id.topDesign).findViewById(R.id.topText);
         topTitle.setText("Enter Pin code");
-        if(Objects.equals(from, "login")) {
+        if (Objects.equals(from, "login")) {
 //            findViewById(R.id.backButton).setVisibility(View.VISIBLE);
 //            findViewById(R.id.backButton).setOnClickListener(v -> {
 //                onBackPressed();
 //            });
-        }else {
+        } else {
 //            findViewById(R.id.backButton).setVisibility(View.GONE);
         }
         findViewById(R.id.adminButton).setOnClickListener(v -> {
@@ -114,19 +128,55 @@ public class SecurityPinActivity extends AppCompatActivity implements SecurityPi
             Type type = new TypeToken<AppDetailsModel.Data>() {
             }.getType();
             try {
-                data = gson.fromJson(SharPrefHelper.getPreferenceData(this,SharPrefHelper.KEY_App_Details), type);
+                data = gson.fromJson(SharPrefHelper.getPreferenceData(this, SharPrefHelper.KEY_App_Details), type);
             } catch (Exception e) {
                 System.out.println("json conversion failed");
             }
             String adminMsg = data.getAdmin_message();
 
-            String url = "https://api.whatsapp.com/send?phone="+SharPrefHelper.getContactDetails(this, SharPrefHelper.KEY_WHATSAPP_NUMBER)+"&text="+adminMsg;
+            String url = "https://api.whatsapp.com/send?phone=" + SharPrefHelper.getContactDetails(this, SharPrefHelper.KEY_WHATSAPP_NUMBER) + "&text=" + adminMsg;
             Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData(Uri.parse(url));
             startActivity(i);
         });
     }
 
+    private void addPinWatcher(EditText pinBox, CheckBox checkBox) {
+        pinBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+                // Not needed for this logic
+            }
+
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                if (charSequence.length() > 0) {
+//                    pinBox.setVisibility(View.GONE);  // Hide the EditText once the user enters something
+//                    checkBox.setVisibility(View.VISIBLE);  // Show the CheckBox
+//                    checkBox.setChecked(true);
+                    if (pinBox == pinBox1 && charSequence.length() == 1) {
+                        pinBox2.requestFocus();
+                    }
+                    if (pinBox == pinBox2 && charSequence.length() == 1) {
+                        pinBox3.requestFocus();
+                    }
+                    if (pinBox == pinBox3 && charSequence.length() == 1) {
+                        pinBox4.requestFocus();
+                    }
+                    Log.e(charSequence.toString(), "");
+                    Log.e("", pinBox4.getText().toString());
+                    if (!pinBox4.getText().toString().isEmpty()) {
+                        checkBoxFun(charSequence.toString());
+                    }
+                    // Mark the CheckBox as checked
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Not needed for this logic
+            }
+        });
+    }
 
     public void button1(View view) {
         checkBoxFun("1");
@@ -158,6 +208,7 @@ public class SecurityPinActivity extends AppCompatActivity implements SecurityPi
 
     public void button8(View view) {
         checkBoxFun("8");
+
     }
 
     public void button9(View view) {
@@ -176,44 +227,65 @@ public class SecurityPinActivity extends AppCompatActivity implements SecurityPi
     }
 
     public void buttonDelete(View view) {
-        if (checkBox1.isChecked()&& checkBox2.isChecked()&& checkBox3.isChecked()&& checkBox4.isChecked()){
+        if (checkBox4.isChecked()) {
             checkBox4.setChecked(false);
-        }else if (checkBox1.isChecked() && checkBox2.isChecked()&& checkBox3.isChecked()&& !checkBox4.isChecked()){
+            checkBox4.setVisibility(View.GONE); // Hide CheckBox when unchecked
+            pinBox4.setVisibility(View.VISIBLE); // Show EditText again
+        } else if (checkBox3.isChecked()) {
             checkBox3.setChecked(false);
-        }else if (checkBox1.isChecked() &&checkBox2.isChecked() && !checkBox3.isChecked()&& !checkBox4.isChecked()){
+            checkBox3.setVisibility(View.GONE); // Hide CheckBox when unchecked
+            pinBox3.setVisibility(View.VISIBLE); // Show EditText again
+        } else if (checkBox2.isChecked()) {
             checkBox2.setChecked(false);
-        }else if (checkBox1.isChecked() && !checkBox2.isChecked()&&!checkBox3.isChecked()&& !checkBox4.isChecked()){
+            checkBox2.setVisibility(View.GONE); // Hide CheckBox when unchecked
+            pinBox2.setVisibility(View.VISIBLE); // Show EditText again
+        } else if (checkBox1.isChecked()) {
             checkBox1.setChecked(false);
+            checkBox1.setVisibility(View.GONE); // Hide CheckBox when unchecked
+            pinBox1.setVisibility(View.VISIBLE); // Show EditText again
         }
     }
-    String pin1, pin2, pin3,pin4;
-    public void checkBoxFun(String pin){
-        if (!checkBox1.isChecked()&& !checkBox2.isChecked()&& !checkBox3.isChecked()&& !checkBox4.isChecked()){
-            checkBox1.setChecked(true);
-            pin1 = pin;
-        }else if (checkBox1.isChecked() && !checkBox2.isChecked()&& !checkBox3.isChecked()&& !checkBox4.isChecked()){
-            checkBox2.setChecked(true);
-            pin2 = pin;
-        }else if (checkBox1.isChecked() &&checkBox2.isChecked() && !checkBox3.isChecked()&& !checkBox4.isChecked()){
-            checkBox3.setChecked(true);
-            pin3 = pin;
-        }else if (checkBox1.isChecked() &&checkBox2.isChecked()&&checkBox3.isChecked()&& !checkBox4.isChecked()){
-            checkBox4.setChecked(true);
-            pin4 = pin;
-            presenter.api(SharPrefHelper.getLogInToken(this),pin1 + pin2 + pin3 + pin4);
-        }
 
+//    String pin1, pin2, pin3, pin4;
+
+    public void checkBoxFun(String pin) {
+        presenter.api(SharPrefHelper.getLogInToken(this), pinBox1.getText().toString() + pinBox2.getText().toString() + pinBox3.getText().toString() + pinBox4.getText().toString()); // Call API with pin
+        // Pin Box 1
+//        if (!checkBox1.isChecked() && !checkBox2.isChecked() && !checkBox3.isChecked() && !checkBox4.isChecked()) {
+//            pin1 = pin;
+//            checkBox1.setVisibility(View.VISIBLE); // Show CheckBox
+//            checkBox1.setChecked(true); // Check the box
+//            pinBox1.setVisibility(View.GONE); // Hide EditText
+//        } else if (checkBox1.isChecked() && !checkBox2.isChecked() && !checkBox3.isChecked() && !checkBox4.isChecked()) {
+//            pin2 = pin;
+//            checkBox2.setVisibility(View.VISIBLE); // Show CheckBox
+//            checkBox2.setChecked(true); // Check the box
+//            pinBox2.setVisibility(View.GONE); // Hide EditText
+//        } else if (checkBox1.isChecked() && checkBox2.isChecked() && !checkBox3.isChecked() && !checkBox4.isChecked()) {
+//            pin3 = pin;
+//            checkBox3.setVisibility(View.VISIBLE); // Show CheckBox
+//            checkBox3.setChecked(true); // Check the box
+//            pinBox3.setVisibility(View.GONE); // Hide EditText
+//        } else if (checkBox1.isChecked() && checkBox2.isChecked() && checkBox3.isChecked() && !checkBox4.isChecked()) {
+//            pin4 = pin;
+//            checkBox4.setVisibility(View.VISIBLE); // Show CheckBox
+//            checkBox4.setChecked(true); // Check the box
+//            pinBox4.setVisibility(View.GONE); // Hide EditText
+//
+//            presenter.api(SharPrefHelper.getLogInToken(this), pin1 + pin2 + pin3 + pin4); // Call API with pin
+//        }
     }
 
     public void whatsapp(View view) {
-        String url ="";
-        if (SharPrefHelper.getContactDetails(this, SharPrefHelper.KEY_WHATSAPP_NUMBER)!=null)
-            url = "https://api.whatsapp.com/send?phone="+SharPrefHelper.getContactDetails(this, SharPrefHelper.KEY_WHATSAPP_NUMBER);
-        else url="1234567890";
+        String url = "";
+        if (SharPrefHelper.getContactDetails(this, SharPrefHelper.KEY_WHATSAPP_NUMBER) != null)
+            url = "https://api.whatsapp.com/send?phone=" + SharPrefHelper.getContactDetails(this, SharPrefHelper.KEY_WHATSAPP_NUMBER);
+        else url = "1234567890";
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(url));
         startActivity(i);
     }
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onRestart() {
@@ -246,10 +318,10 @@ public class SecurityPinActivity extends AppCompatActivity implements SecurityPi
 
     @Override
     public void ApiResponse(LoginModel loginModel) {
-        if (loginModel.getStatus().equals(getString(R.string.success))){
+        if (loginModel.getStatus().equals(getString(R.string.success))) {
             SharPrefHelper.setLoginToken(this, loginModel.getData().getToken());
             presenter.doUserDetailsApi(loginModel.getData().getToken());
-        }else {
+        } else {
             checkBox1.setChecked(false);
             checkBox2.setChecked(false);
             checkBox3.setChecked(false);
@@ -270,23 +342,23 @@ public class SecurityPinActivity extends AppCompatActivity implements SecurityPi
 
     @Override
     public void onAppDetailsApiResponse(AppDetailsModel.Data data) {
-        SharPrefHelper.setPreferenceData(this,SharPrefHelper.KEY_MARQUEE_TEXT,data.getBanner_marquee());
-        SharPrefHelper.setPreferenceData(this,SharPrefHelper.KEY_WELCOME_MSG,data.getWelcome_message());
-        SharPrefHelper.setContactDetails(this,SharPrefHelper.KEY_CONTACT_NUMBER1,"+91"+data.getContact_details().getMobile_no_1());
-        SharPrefHelper.setContactDetails(this,SharPrefHelper.KEY_CONTACT_NUMBER2,"+91"+data.getContact_details().getMobile_no_2());
-        SharPrefHelper.setContactDetails(this,SharPrefHelper.KEY_WHATSAPP_NUMBER,"+91"+data.getContact_details().getWhatsapp_no());
-        SharPrefHelper.setContactDetails(this,SharPrefHelper.KEY_WITHDRAW_PROOF,data.getContact_details().getWithdraw_proof());
-        SharPrefHelper.setContactDetails(this,SharPrefHelper.KEY_CONTACT_EMAIL,data.getContact_details().getEmail_1());
-        SharPrefHelper.setBannerImages(this,SharPrefHelper.KEY_BANNER_IMAGES1, data.getBanner_image().getBanner_img_1());
-        SharPrefHelper.setBannerImages(this,SharPrefHelper.KEY_BANNER_IMAGES2, data.getBanner_image().getBanner_img_2());
-        SharPrefHelper.setBannerImages(this,SharPrefHelper.KEY_BANNER_IMAGES3, data.getBanner_image().getBanner_img_3());
-        SharPrefHelper.setBooleanData(this,SharPrefHelper.KEY_MAIN_MARKET_STATUS, data.getProject_status().getMain_market().equals("On"));
-        SharPrefHelper.setBooleanData(this,SharPrefHelper.KEY_STARLINE_MARKET_STATUS, data.getProject_status().getStarline_market().equals("On"));
-        SharPrefHelper.setBooleanData(this,SharPrefHelper.KEY_GALIDESAWAR_MARKET_STATUS, data.getProject_status().getGalidesawar_market().equals("On"));
-        SharPrefHelper.setBooleanData(this,SharPrefHelper.KEY_BANNER_STATUS, data.getProject_status().getBanner_status().equals("On"));
-        SharPrefHelper.setBooleanData(this,SharPrefHelper.KEY_MARQUEE_STATUS, data.getProject_status().getMarquee_status().equals("On"));
-        SharPrefHelper.setPreferenceData(this,SharPrefHelper.KEY_BANNER_LIST, new Gson().toJson(data.getBannerList()));
-        SharPrefHelper.setPreferenceData(this,SharPrefHelper.KEY_App_Details, new Gson().toJson(data));
+        SharPrefHelper.setPreferenceData(this, SharPrefHelper.KEY_MARQUEE_TEXT, data.getBanner_marquee());
+        SharPrefHelper.setPreferenceData(this, SharPrefHelper.KEY_WELCOME_MSG, data.getWelcome_message());
+        SharPrefHelper.setContactDetails(this, SharPrefHelper.KEY_CONTACT_NUMBER1, "+91" + data.getContact_details().getMobile_no_1());
+        SharPrefHelper.setContactDetails(this, SharPrefHelper.KEY_CONTACT_NUMBER2, "+91" + data.getContact_details().getMobile_no_2());
+        SharPrefHelper.setContactDetails(this, SharPrefHelper.KEY_WHATSAPP_NUMBER, "+91" + data.getContact_details().getWhatsapp_no());
+        SharPrefHelper.setContactDetails(this, SharPrefHelper.KEY_WITHDRAW_PROOF, data.getContact_details().getWithdraw_proof());
+        SharPrefHelper.setContactDetails(this, SharPrefHelper.KEY_CONTACT_EMAIL, data.getContact_details().getEmail_1());
+        SharPrefHelper.setBannerImages(this, SharPrefHelper.KEY_BANNER_IMAGES1, data.getBanner_image().getBanner_img_1());
+        SharPrefHelper.setBannerImages(this, SharPrefHelper.KEY_BANNER_IMAGES2, data.getBanner_image().getBanner_img_2());
+        SharPrefHelper.setBannerImages(this, SharPrefHelper.KEY_BANNER_IMAGES3, data.getBanner_image().getBanner_img_3());
+        SharPrefHelper.setBooleanData(this, SharPrefHelper.KEY_MAIN_MARKET_STATUS, data.getProject_status().getMain_market().equals("On"));
+        SharPrefHelper.setBooleanData(this, SharPrefHelper.KEY_STARLINE_MARKET_STATUS, data.getProject_status().getStarline_market().equals("On"));
+        SharPrefHelper.setBooleanData(this, SharPrefHelper.KEY_GALIDESAWAR_MARKET_STATUS, data.getProject_status().getGalidesawar_market().equals("On"));
+        SharPrefHelper.setBooleanData(this, SharPrefHelper.KEY_BANNER_STATUS, data.getProject_status().getBanner_status().equals("On"));
+        SharPrefHelper.setBooleanData(this, SharPrefHelper.KEY_MARQUEE_STATUS, data.getProject_status().getMarquee_status().equals("On"));
+        SharPrefHelper.setPreferenceData(this, SharPrefHelper.KEY_BANNER_LIST, new Gson().toJson(data.getBannerList()));
+        SharPrefHelper.setPreferenceData(this, SharPrefHelper.KEY_App_Details, new Gson().toJson(data));
 
     }
 
@@ -295,8 +367,8 @@ public class SecurityPinActivity extends AppCompatActivity implements SecurityPi
         SharPrefHelper.setSignUpData(this, SharPrefHelper.KEY_PERSON_NAME, data.getUsername());
         SharPrefHelper.setLoginSuccess(this, true);
         Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("from","pin");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra("from", "pin");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
     }
@@ -316,9 +388,9 @@ public class SecurityPinActivity extends AppCompatActivity implements SecurityPi
     }
 
     public void resetPin(View view) {
-        if (mobileNumber == null){
+        if (mobileNumber == null) {
             presenter.resetPinBtn(SharPrefHelper.getSignUpData(this, SharPrefHelper.KEY_MOBILE_NUMBER));
-        }else {
+        } else {
             presenter.resetPinBtn(mobileNumber);
         }
     }
@@ -327,7 +399,7 @@ public class SecurityPinActivity extends AppCompatActivity implements SecurityPi
     public void backToLogin(View view) {
         SharPrefHelper.setClearData(this);
         Intent intent = new Intent(this, SplashActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
     }
